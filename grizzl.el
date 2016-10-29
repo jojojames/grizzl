283849819f19fe10634ba83b1cec3733b85d6e6c
James Nguyen <ja.nguyen@gmail.com>
2016-10-29 16:00:13 -0700

    Format file

--- grizzl.el
+++ grizzl.el
@@ -161,7 +161,7 @@ If the :END option is specified, up to :END results are returned."
 
 (defun grizzl-cons-result (term matches results)
   "Build a new result for TERM and hash-table MATCHES consed with RESULTS."
-    (cons (cons term matches) results))
+  (cons (cons term matches) results))
 
 (defun grizzl-rewind-result (term index result)
   "Adjusts RESULT according to TERM, ready for a new search."
@@ -171,7 +171,7 @@ If the :END option is specified, up to :END results are returned."
              (old-len (length old-term)))
         (if (and (>= new-len old-len)
                  (string-equal old-term (substring term 0 old-len)))
-              result
+            result
           (grizzl-rewind-result term index (cdr result))))
     (grizzl-cons-result "" (grizzl-base-matches index) nil)))
 
@@ -225,9 +225,9 @@ If the :END option is specified, up to :END results are returned."
 (defun grizzl-search-increment (sub-table result)
   "Use the search lookup table to filter already-accumulated results."
   (cl-flet ((next-offset (key current sub-table)
-              (cl-find-if (lambda (v)
-                         (> v current))
-                       (gethash key sub-table))))
+                         (cl-find-if (lambda (v)
+                                       (> v current))
+                                     (gethash key sub-table))))
     (maphash (lambda (k v)
                (let* ((oldpos (car v))
                       (oldrank (cadr v))
@@ -271,7 +271,7 @@ If the :END option is specified, up to :END results are returned."
 (defface grizzl-prompt-face
   `((t :inherit 'mode-line-inactive))
   "Face used for grizzl prompt."
-   :group 'grizzl-mode)
+  :group 'grizzl-mode)
 
 ;;; --- Minor Mode Definition
 
